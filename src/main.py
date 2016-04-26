c182f6b61e4c3d0dc1dc1ce39924afd2c068d13a
Michael Allen II <michaelkallen@gmail.com>
2016-04-26 07:10:40 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+
+GOPRO_MAIN_FILE_PREFIX = 'GOPR'
+GOPRO_SHORTENED_FILE_PREFIX = 'GP'
+
+
+def main(args):
+    if len(args) < 2:
+        print('Error, missing argument for folder')
+        sys.exit(1)
+    folder_location = args[1]
+    if folder_location[-1] == '/':
+        folder_location = folder_location[0:len(folder_location)-1]
+
+    files = os.listdir(folder_location)
+    file_groupings = dict()
+    for file in files:
+        if os.path.isdir(file):
+            continue
+        numbers = file[len(GOPRO_MAIN_FILE_PREFIX):file.rfind('.')]
+        if file.startswith(GOPRO_MAIN_FILE_PREFIX):
+            numbers_list = list()
+            numbers_list.append(file)
+            file_groupings[numbers] = numbers_list
+        elif file.startswith(GOPRO_SHORTENED_FILE_PREFIX):
+            file_groupings[numbers].append(file)
+
+    for k, v in file_groupings.items():
+        if len(v) < 2:
+            continue
+        new_folder = folder_location + '/' + GOPRO_MAIN_FILE_PREFIX + str(k)
+        if not os.path.isdir(new_folder):
+            os.mkdir(new_folder)
+        for val in v:
+            new_file_path = new_folder + '/' + val
+            old_file_path = folder_location + '/' + val
+            os.rename(old_file_path, new_file_path)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
